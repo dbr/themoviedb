88a0ce84fd3724c56a00deaab985ae45a76238bf
dbr <dbr.onix@gmail.com>
2010-06-12 16:18:01 +0930

    Rewrite tests for nosetests. Also adds a test for the MovieResult.info method, and updates the example getHashInfo test

--- test_tmdb.py
+++ test_tmdb.py
@@ -3,156 +3,133 @@
 #author:dbr/Ben
 #project:themoviedb
 
-import unittest
-
 import tmdb
 
-class test_search(unittest.TestCase):
-    def setUp(self):
-        self.m = tmdb.MovieDb()
 
-    def test_simple_search(self):
-        """Simple test search
-        """
-        self.assertEquals(
-            type(
-                self.m.search("Fight Club")
-            ),
-            tmdb.SearchResults
-        )
+def test_simple_search():
+    """Simple test search
+    """
+    t = tmdb.MovieDb()
+    sr = t.search("Fight Club")
+    assert isinstance(sr, tmdb.SearchResults)
+    assert sr[0]['name'] == "Fight Club"
 
-    def test_search_results(self):
-        """Check SearchResults are usable
-        """
-        results = self.m.search("Fight Club")
-        first_result = results[0]
 
-        self.assertEquals(
-            type(first_result),
-            tmdb.MovieResult
-        )
+def test_search_results():
+    """Check SearchResults are usable
+    """
+    t = tmdb.MovieDb()
+    results = t.search("Fight Club")
+    first_result = results[0]
 
-        self.assertEquals(
-            first_result['name'],
-            'Fight Club'
-        )
+    assert isinstance(first_result, tmdb.MovieResult)
 
-        self.assertEquals(
-            first_result['released'],
-            '1999-09-16'
-        )
+    assert first_result['name'] == 'Fight Club'
 
-        self.assertEquals(
-            first_result['imdb_id'],
-            'tt0137523'
-        )
+    assert first_result['released'] == '1999-09-16'
 
-class test_getmovieinfo(unittest.TestCase):
-    def test_search_to_info(self):
-        """Gets a movie ID via search, then calls getMovieInfo using this
-        """
-        sr = tmdb.search("fight club")[0]
-        movie = tmdb.getMovieInfo(sr['id'])
-        self.assertEquals(
-            sr['name'],
-            movie['name']
-        )
-    
-    def test_get_director(self):
-        """Checks you can get the director of a film
-        """
-        mid = tmdb.search("Inglourious Basterds")[0]['id']
-        movie = tmdb.getMovieInfo(mid)
+    assert first_result['imdb_id'] == 'tt0137523'
 
-        self.assertTrue(len(movie['cast']['director']) == 1)
-        self.assertEquals(
-            movie['cast']['director'][0]['name'],
-            "Quentin Tarantino"
-        )
 
-class test_wrappers(unittest.TestCase):
-    def test_search_wrapper(self):
-        """Tests tmdb.search() wrapper works correctly
-        """
-        r = tmdb.search("The Matrix")
-        self.assertEquals(
-            type(r),
-            tmdb.SearchResults
-        )
+def test_info_from_search():
+    """Get info from first search result
+    """
+    t = tmdb.MovieDb()
+    results = t.search("Fight Club")
+    first_result = results[0]
+    info = first_result.info()
+    assert info['name'] == "Fight Club"
 
-    def test_getmovieinfo_wrapper(self):
-        """Tests tmdb.getMovieInfo() wrapper works correctly
-        """
-        r = tmdb.getMovieInfo(550)
-        self.assertEquals(
-            type(r),
-            tmdb.Movie
-        )
 
-class test_artwork(unittest.TestCase):
-    def setUp(self):
-        filmId = tmdb.MovieDb().search("Fight Club")[0]['id']
-        self.film = tmdb.MovieDb().getMovieInfo(filmId)
+def test_search_to_info():
+    """Gets a movie ID via search helper, then calls getMovieInfo using this
+    """
+    sr = tmdb.search("fight club")[0]
+    movie = tmdb.getMovieInfo(sr['id'])
+    assert sr['name'] == movie['name']
 
-    def test_poster_urls(self):
+
+def test_get_director():
+    """Checks you can get the director of a film
+    """
+    mid = tmdb.search("Inglourious Basterds")[0]['id']
+    movie = tmdb.getMovieInfo(mid)
+
+    assert len(movie['cast']['director']) == 1
+    assert movie['cast']['director'][0]['name'] == "Quentin Tarantino"
+
+
+def test_search_wrapper():
+    """Tests tmdb.search() wrapper works correctly
+    """
+    r = tmdb.search("The Matrix")
+    assert isinstance(r, tmdb.SearchResults)
+
+
+def test_getmovieinfo_wrapper():
+    """Tests tmdb.getMovieInfo() wrapper works correctly
+    """
+    r = tmdb.getMovieInfo(550)
+    assert isinstance(r, tmdb.Movie)
+
+
+def test_artwork_generator():
+    """Generates several tests posters/backdrops
+
+    Uses test generator to prevent multiple requests for the movie info
+    """
+    filmId = tmdb.MovieDb().search("Fight Club")[0]['id']
+    film = tmdb.MovieDb().getMovieInfo(filmId)
+
+    def test_poster_urls():
         """Checks posters are valid looking URLs
         """
-        for poster in self.film['images'].posters:
+        for poster in film['images'].posters:
             for key, value in poster.items():
                 if key not in ['id', 'type']:
-                    self.assertTrue(
-                        value.startswith("http://")
-                    )
+                    assert value.startswith("http://")
 
-    def test_backdrop_urls(self):
+    yield test_poster_urls
+
+    def test_backdrop_urls():
         """Checks backdrop images are valid looking URLs
         """
-        for poster in self.film['images'].posters:
+        for poster in film['images'].posters:
             for key, value in poster.items():
                 if key not in ['id', 'type']:
-                    self.assertTrue(
-                        value.startswith("http://")
-                    )
+                    assert value.startswith("http://")
 
-    def test_artwork_repr(self):
+    yield test_backdrop_urls
+
+    def test_artwork_repr():
         """Checks artwork repr looks sensible
         """
-        self.assertTrue(
-            repr(self.film['images'].posters[0]).startswith(
-                "<Image (poster for ID"
-            )
-        )
-        self.assertTrue(
-            repr(self.film['images'].backdrops[0]).startswith(
-                "<Image (backdrop for ID"
-            )
-        )
+        poster_repr = repr(film['images'].posters[0])
+        assert poster_repr.startswith("<Image (poster for ID")
 
-    def test_posters(self):
+        backdrop_repr = repr(film['images'].backdrops[0])
+        assert backdrop_repr.startswith("<Image (backdrop for ID")
+
+    yield test_artwork_repr
+
+    def test_posters():
         """Check retrieving largest artwork
         """
-        self.assertTrue(
-            len(self.film['images'].posters) > 1
-        )
-        self.assertTrue(
-            len(self.film['images'].backdrops) > 1
-        )
-        
-        self.assertEquals(
-            self.film['images'].posters[0]['original'],
-            'http://images.themoviedb.org/posters/4861/Fight_Club.jpg'
-        )
+        assert len(film['images'].posters) > 1
+        assert len(film['images'].backdrops) > 1
+        print film['images'].posters[0]['cover']
+        url = film['images'].posters[0]['cover'],
+        assert url == 'http://images.themoviedb.org/posters/4861/Fight_Club.jpg'
+
+    yield test_posters
 
 
-class test_hashing(unittest.TestCase):
-    def setUp(self):
-        self.m = tmdb.MovieDb()
-
-    def test_gethashinfo(self):
-        """Tests searching by file hash
-        """
-        film = self.m.hashGetInfo('00277ff46533b155')
-        self.assertEquals(film['name'], 'Willow')
+def test_gethashinfo():
+    """Tests searching by file hash
+    """
+    t = tmdb.MovieDb()
+    film = t.hashGetInfo('00277ff46533b155')
+    assert film['name'] == 'Underworld'
 
 
 if __name__ == '__main__':
