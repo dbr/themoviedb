cb99544152665f3646a94b55923091d4623be6e3
dbr <dbr.onix@gmail.com>
2011-08-07 14:07:38 +0930

    Use os.path.size instead of os.stat

--- tmdb.py
+++ tmdb.py
@@ -436,7 +436,7 @@ def mediaGetInfo(hash, size):
 def searchByHashingFile(filename):
     """Searches for the specified file using the OpenSubtitle hashing method
     """
-    return mediaGetInfo(opensubtitleHashFile(filename), os.path.size(filename))
+    return mediaGetInfo(opensubtitleHashFile(filename), os.stat(filename).st_size)
 
 
 def main():
